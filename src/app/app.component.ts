5bd7766e3b88d1eac1afe582e305e9508cf5c171
Gaga Demetrashvili <gaga.demetrashvili@makingscience.com>
2023-03-22 20:23:46 +0400

    complete task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
+  firstSeeAll: boolean = false;
+  secondSeeAll: boolean = false;
   numbers = [
+    {
+      isSelected: false,
+    },
+    {
+      isSelected: false,
+    },
+    {
+      isSelected: false,
+    },
+    {
+      isSelected: false,
+    },
     {
       isSelected: true,
     },
-    {
-      isSelected: false,
-    },
-    {
-      isSelected: false,
-    },
-    {
-      isSelected: false,
-    },
-    {
-      isSelected: false,
-    },
-  ];
-  meOrOther: 'forMe' | 'forOther' = 'forMe';
-  data = [
-    {
-      isSelected: false,
-      name: 'ბიძინა თაბაგარი',
-      policyNum: 'OPPMED 5023234123',
-    },
-    {
-      isSelected: false,
-      name: 'ლექსო ნიჟარაძე',
-      policyNum: 'OPPMED 5023234123',
-    },
-    {
-      isSelected: false,
-      name: 'უჩა ზერაგია',
-      policyNum: 'OPPMED 5023234123',
-    },
   ];
   title = 'GPI';
-  changeStyles(index: number) {
-    this.data.map((obj: any) => (obj.isSelected = false));
-    this.data[index].isSelected = true;
-  }
-
-  ngOnInit(): void {
-    this.meOrOther = 'forMe';
-  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html, ts}",
-  ],
+  content: ["./src/**/*.{html, ts}"],
   theme: {
     theme: {
       screens: {
         sm: "480px",
         md: "768px",
         lg: "976px",
-        xl: "1440px"
+        xl: "1440px",
       },
     },
     extend: {
       colors: {
-        pinkCustom: "#ed1c7c",
-        paleGreen: "#9cd6a5",
-        paleBg: "#f1f4ff",
-        numBg: "#e9e8ef",
+        deepPurple: "#322a68",
+        customPink: "#ed1c7c",
+        customGreen: "#22a336",
+        customGray: "#f1f4ff",
         textColor: "#2b2763",
         paleTextColor: "#9595b6",
         gray: "#1616161A",
@@ -28,50 +26,49 @@ module.exports = {
         calBorder: "#DEE0E8",
         doctorName: "#292562",
         address: "#9199B4",
-        timeBackgroundColor: '#F4F4FE',
-        white: '#FFFFFF'
-
+        timeBackgroundColor: "#F4F4FE",
+        white: "#FFFFFF",
       },
       fontSize: {
-        7: '14px'
+        7: "14px",
       },
       spacing: {
-        1: '2px',
-        2: '4px',
-        4: '8px',
-        8: '16px',
-        5: '10px',
-        6: '12px',
-        9: '18px',
-        10: '20px',
-        11: '22px',
-        12: '24px',
-        13: '26px',
-        14: '28px',
-        16: '32px',
-        18: '36px',
-        20: '40px',
-        21: '42px',
-        23: '46px',
-        34: '68px',
-        36: '72px',
-        39: '78px',
-        41: '82px',
-        47: '94px',
-        100: '200px',
-        140: '280px',
-        150: '300px',
-        584: '1168px'
+        1: "2px",
+        2: "4px",
+        4: "8px",
+        8: "16px",
+        5: "10px",
+        6: "12px",
+        9: "18px",
+        10: "20px",
+        11: "22px",
+        12: "24px",
+        13: "26px",
+        14: "28px",
+        16: "32px",
+        18: "36px",
+        20: "40px",
+        21: "42px",
+        23: "46px",
+        34: "68px",
+        36: "72px",
+        39: "78px",
+        41: "82px",
+        47: "94px",
+        100: "200px",
+        140: "280px",
+        150: "300px",
+        584: "1168px",
       },
       boxShadow: {
-        header: '0px 0px 6px #1616161A',
+        header: "0px 0px 6px #1616161A",
       },
       borderRadius: {
-        4: '8px',
-        6: '12px',
-        7: '14px'
-      }
+        4: "8px",
+        6: "12px",
+        7: "14px",
+      },
     },
   },
   plugins: [],
-}
+};
